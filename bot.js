fc188aa3ca9ee70498312456af7e26ecbb2e8dd3
pythoncodess <45357572+pythoncodess@users.noreply.github.com>
2018-12-05 18:32:23 +0000

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,20 @@
+/*
+ * Bot Made By Master Joker
+ * ;-;
+ * Nothing xD
+ */
+
+const Files = require(require('path').join(__dirname, 'js', 'Main.js')).Files;
+
+if(!process.send) {
+
+Files.initStandalone();
+
+} else {
+
+process.on('message', function(content) {
+	Files.initBotTest(content);
+});
+
+
+}
